07ff459f474ef39ea31b12bf4982e81254e95260
Hayden McSorley <haymcsorley@gmail.com>
2023-04-12 17:23:41 -0500

    Add places array and sample methods

--- friends.rb
+++ friends.rb
@@ -2,4 +2,11 @@ arrays = ["jon", "eli", "ian", "ben", "sue"]
     
 puts arrays[2]
 puts arrays[4]
-puts arrays[0]
+puts arrays[0], ""
+
+places = ["denver", "boulder", "austin", "disney world", "the moon"]
+
+places.pop
+puts places, ""
+places.push("mars")
+puts places, ""
